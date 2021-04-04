9f8121e7ba9a8817f9bcb4f261daba4e01393814
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2021-04-04 20:58:30 +0900

    like 기능 완료 (추천 액션, 출력)

--- BBS/src/bbs/Bbs.java
+++ BBS/src/bbs/Bbs.java
@@ -5,7 +5,7 @@ public class Bbs {
 	private int bbsID; //게시글 번호
 	private String bbsTitle; //게시글 제목
 	private String userID; //사용자 아이디
-	private String bbsDate; //게시글 업로드 날짜
+	private String bbsDate; //게시글 업로드 날짜 
 	private String bbsContent; //게시글 내용
 	private int bbsAvailable; //게시글의 삭제 유무
 	private int likeCount; //게시글 추천 갯수
